0701051f0e69eb5cfef96c1acf24e2efb10c0556
Krishna Kola <kkola@digitalinfuzion.com>
2019-07-24 13:31:10 -0400

    update spreadsheet and add controls to overlay

---
+++ controls/overlay.rb
@@ -0,0 +1,167 @@
+include_controls 'canonical-ubuntu-16.04-lts-stig-baseline' do
+  control 'V-75459' do
+
+  end
+  control 'V-75465' do
+
+  end
+  control 'V-75475' do
+
+  end
+  control 'V-75485' do
+
+  end
+  control 'V-75489' do
+
+  end
+  control 'V-75491' do
+
+  end
+  control 'V-75505' do
+
+  end
+  control 'V-75507' do
+
+  end
+  control 'V-75511' do
+
+  end
+  control 'V-75527' do
+
+  end
+  control 'V-75529' do
+
+  end
+  control 'V-75553' do
+
+  end
+  control 'V-75555' do
+
+  end
+  control 'V-75557' do
+
+  end
+  control 'V-75605' do
+
+  end
+  control 'V-75607' do
+
+  end
+  control 'V-75609' do
+
+  end
+  control 'V-75611' do
+
+  end
+  control 'V-75613' do
+
+  end
+  control 'V-75615' do
+
+  end
+  control 'V-75623' do
+
+  end
+  control 'V-75631' do
+
+  end
+  control 'V-75633' do
+
+  end
+  control 'V-75659' do
+
+  end
+  control 'V-75709' do
+
+  end
+  control 'V-75711' do
+
+  end
+  control 'V-75713' do
+
+  end
+  control 'V-75715' do
+
+  end
+  control 'V-75717' do
+
+  end
+  control 'V-75719' do
+
+  end
+  control 'V-75721' do
+
+  end
+  control 'V-75723' do
+
+  end
+  control 'V-75725' do
+
+  end
+  control 'V-75727' do
+
+  end
+  control 'V-75803' do
+
+  end
+  control 'V-75807' do
+
+  end
+  control 'V-75811' do
+
+  end
+  control 'V-75813' do
+
+  end
+  control 'V-75825' do
+
+  end
+  control 'V-75829' do
+
+  end
+  control 'V-75831' do
+
+  end
+  control 'V-75837' do
+
+  end
+  control 'V-75859' do
+
+  end
+  control 'V-75863' do
+
+  end
+  control 'V-75867' do
+
+  end
+  control 'V-75869' do
+
+  end
+  control 'V-75903' do
+
+  end
+  control 'V-75905' do
+
+  end
+  control 'V-75907' do
+
+  end
+  control 'V-75911' do
+
+  end
+  control 'V-78005' do
+
+  end
+  control 'V-78007' do
+
+  end
+  control 'V-80959' do
+
+  end
+  control 'V-80961' do
+
+  end
+  control 'V-80965' do
+
+  end
+end
